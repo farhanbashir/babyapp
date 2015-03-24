c8cd4a2c3fb18bc2d41697aa8882e5d756001353
muhammad farhan bashir <farhan.bashir2002@gmail.com>
2015-03-24 23:51:00 +0500

    config change

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 $config = array();
 
 $config["db"]["db_host"] = "localhost";
-$config["db"]["db_name"] = "babyapp";
-$config["db"]["db_user"] = "root";
-$config["db"]["db_password"] = "root";
+$config["db"]["db_name"] = "farhan_babyapp";
+$config["db"]["db_user"] = "farhan_economic";
+$config["db"]["db_password"] = "tigragroup3";
 $config["google_key"] = 'AIzaSyA9mpLCBdpUBaGcgnSeTckigujj8FAQhy8';
 $config["REMOTE_SOCKET_GOOGLE"] = 'https://android.googleapis.com/gcm/send';
 $config["PASS_PHRASE"] = "waslapp";
